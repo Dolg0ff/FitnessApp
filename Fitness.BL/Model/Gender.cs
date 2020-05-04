dfedf1d784f73b28992dee42ce567c0e763bce92
Yuriy <ys.dolgoff@gmail.com>
2020-05-04 16:21:21 +0300

    Create user model

---
+++ Fitness.BL/Model/Gender.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fitness.BL.Model
+{
+    /// <summary>
+    /// Пол
+    /// </summary>
+    public class Gender
+    {
+        public string Name { get; }
+        /// <summary>
+        /// Создать новый пол
+        /// </summary>
+        /// <param name="name">название пола</param>
+
+        public Gender(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentNullException("Имя пола не может быть пустым или null");
+            }
+
+            Name = name;
+        }
+    }
+}
